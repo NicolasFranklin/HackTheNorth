c7e8d17764e05af10245e23ed8d45461fe43cbc6
Eric Wang <the.ericwang35@gmail.com>
2018-09-16 02:09:41 -0400

    adjust zoom again

--- dist/index.js
+++ dist/index.js
@@ -47,7 +47,7 @@ let map = new H.Map(
     defaultLayers.normal.map,
     {
         pixelRatio,
-        center: new H.geo.Point(34.62242, -78.60813),
+        center: new H.geo.Point(34.5242, -78.60813),
         zoom:10
     }
 );
